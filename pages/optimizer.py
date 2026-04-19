9bc446f28909e4b44d5734bef2c50beb4841ee24
Manuela Viviana Chacon <47308049+mavivi95@users.noreply.github.com>
2026-04-18 23:36:32 -0500

    Update Streamlit column method from beta_columns to columns

--- pages/optimizer.py
+++ pages/optimizer.py
@@ -7,7 +7,7 @@ def app():
       st.header('Modelo matemático')
       st.write('En el contexto de las redes neuronales los optimizadores son algoritmos que permiten realizar el proceso de optimización de la función de pérdidas, con el propósito de encontrar los pesos adecuados para las conexiones de las redes neuronales. ')
       st.markdown("""---""")  
-      col_adagrad, col_rmsprop= st.beta_columns(2)
+      col_adagrad, col_rmsprop= st.columns(2)
       with col_adagrad:
         st.subheader('Adagrad')
         st.markdown('<div style="text-align: justify"> Intuitivamente el método disminuye la tasa de aprendizaje en función de los valores históricos del cuadrado del gradiente. Cuanto menor sea el gradiente acumulado, menor será el valor de v , lo que conducirá a una mayor tasa de aprendizaje. Después de varias iteraciones, los parámetros frecuentes comenzarán a recibir actualizaciones muy pequeñas debido a la disminución de la tasa de aprendizaje. </div>', unsafe_allow_html=True)
@@ -18,7 +18,7 @@ def app():
         st.markdown('<div style="text-align: justify"> El método toma la filosofía del algoritmo AdaGram con la diferencia que incluye un término que evita la acumulación excesiva de los valores del cuadrado del gradiente. La acumulación excesiva se corrige con un promedio exponencial ponderado, este promedio se pondera según la proximidad de los valores históricos.</div>', unsafe_allow_html=True)
         st.latex(r'''v_t = \beta v_{t-1}+ (1-\beta) \nabla w_t^2''')
         st.latex(r'''w_{t+1} = w_t-\frac{\eta}{\sqrt{v_t + \epsilon}} \nabla w_t''')
-      col_adam, col_adadelta= st.beta_columns(2)
+      col_adam, col_adadelta= st.columns(2)
       with col_adam:
         st.subheader('Adam')
         st.markdown('<div style="text-align: justify"> Desde el enfoque físico el algoritmo puede interpretarse como incluir los factores de momento y fricción. La idea física es "soltar una bola pesada por la superficie (función a optimizar) y tener en cuenta que se desliza por la mayor pendiente (gradiente) pero se tiene en cuenta la inercia y la fricción El algoritmo emplea como primer momento el promedio de los gradientes  (inercia) y segundo momento la variancia (fricción). Los pesos se actualizan en función del decaimiento exponencial de ambos momentos. </div>', unsafe_allow_html=True)
